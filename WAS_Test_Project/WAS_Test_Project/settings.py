865c4792e62c51fe9096e8120f41ff93bc431f65
Alicolliar <alicolliar@gmail.com>
2022-03-22 15:52:55 +0000

    Mopping somethings up

--- WAS_Test_Project/WAS_Test_Project/settings.py
+++ WAS_Test_Project/WAS_Test_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x!&av=t$&e!x4$5f+947ld90*fj3rvo1gdn-8*7d=qq)1mxr=u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alicolliar.pythonanywhere.com']
 
 
 # Application definition
